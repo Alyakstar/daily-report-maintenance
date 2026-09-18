6c877910f280593f1f9015247ffe59ec490adb73
Alyakstar <147696956+Alyakstar@users.noreply.github.com>
2026-09-18 11:42:52 +0700

    Add Vercel deployment support
    for deploying in vercel app

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/daily-report-maintenance/",
+  base: process.env.VERCEL
+    ? "/"
+    : "/daily-report-maintenance/",
 
   server: {
     port: 5173,
